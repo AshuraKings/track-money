fc918c080ab8ec63d69d9c04577b6ca37b6f50e9
Achmad Rifa'i <janoko.java22@yahoo.com>
2025-02-22 16:37:06 +0700

    add darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
+<script setup>
+import { useDarkMode } from './stores/darkmode'
+
+const darkmode = useDarkMode()
+
+function toDark() {
+  darkmode.reverseTheme()
+}
+</script>
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <h1 class="text-3xl font-bold underline text-black dark:text-gray-200">
+      Hello world!
+    </h1>
+    <button @click="toDark" class="text-black dark:text-gray-200">
+      To Dark
+    </button>
+  </div>
 </template>
