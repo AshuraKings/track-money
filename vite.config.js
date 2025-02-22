33ede0355c1acb402e0ffd0faf411d208cf92eb7
Achmad Rifa'i <janoko.java22@yahoo.com>
2025-02-22 16:10:13 +0700

    add tailwind

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: id => {
+          if (id.includes('node_modules')) {
+            console.log(id)
+            for (const s of []) if (id.includes('/node_modules/' + s)) {
+              return s
+            }
+            for (const s of [' vue']) if (id.includes('/node_modules/@' + s)) {
+              return s
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
