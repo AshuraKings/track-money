4b8192243be063b05cef331f80c72a17405a6a1f
Achmad Rifa'i <janoko.java22@yahoo.com>
2025-02-22 17:05:51 +0700

    add icons

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import './icons'
+import { OhVueIcon } from "oh-vue-icons"
 
 createApp(App)
     .use(createPinia())
+    .component('v-icon', OhVueIcon)
     .mount('#app')
