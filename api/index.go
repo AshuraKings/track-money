68082e4a0250d677a63608f756142ee3177c0c09
Achmad Rifa'i <janoko.java22@yahoo.com>
2025-02-23 11:54:53 +0700

    serverless integrated

--- api/index.go
+++ api/index.go
@@ -1,10 +1,15 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	resp := map[string]interface{}{}
+	resp["msg"] = "Hello World"
+	jsonData, _ := json.Marshal(resp)
+	w.Header().Add("Content-Type", "application/json")
+	fmt.Fprint(w, string(jsonData))
 }
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useDarkMode } from './stores/darkmode'
 
-const darkmode = useDarkMode()
+const darkmode = useDarkMode(), msg = ref('')
 
 const iconBtn = computed(() => darkmode.isDark ? 'md-sunny' : 'fa-moon')
 
@@ -12,12 +12,16 @@ function toDark() {
 
 onMounted(() => {
   document.title = 'App'
+  fetch('/api', { method: 'GET' })
+    .then(r => r.json())
+    .then(r => msg.value = r.msg)
+    .catch(console.log)
 })
 </script>
 <template>
   <div>
     <h1 class="text-3xl font-bold underline text-black dark:text-gray-200">
-      Hello world!
+      {{ msg }}
     </h1>
     <button @click="toDark" class="text-black dark:text-gray-200">
       <v-icon :name="iconBtn" />
