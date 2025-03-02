313fb9c39cc3d06f50247e3b3ee840472696c413
Achmad Rifa'i <janoko.java22@yahoo.com>
2025-03-02 22:16:32 +0700

    user profiling done

---
+++ src/components/MessageModal.vue
@@ -0,0 +1,27 @@
+<script setup>
+import BaseModal from '../layouts/BaseModal.vue'
+
+defineProps({
+    open: Boolean,
+    title: String,
+    message: String,
+    severity: {
+        validator(value, props) {
+            return ['success', 'warning', 'danger'].includes(value)
+        }
+    }
+})
+</script>
+<template>
+    <BaseModal @onClose="$emit('onClose')" :open="open">
+        <template #header>
+            <h3 class="text-xl font-semibold dark:text-white">{{ title }}</h3>
+        </template>
+        <div class="text-center">
+            <v-icon v-if="severity === 'success'" name="io-checkmark-sharp" class="w-32 h-32 text-primary-500" />
+            <v-icon v-else name="md-cancel-outlined"
+                :class="`w-32 h-32 text-${severity === 'danger' ? 'red' : 'yellow'}-500`" />
+            <p class="text-black dark:text-white">{{ message }}</p>
+        </div>
+    </BaseModal>
+</template>
---
+++ src/components/NoAuthInput.vue
@@ -0,0 +1,23 @@
+<script setup>
+const model = defineModel({ type: String })
+defineProps({
+    label: String,
+    name: {
+        required: true,
+        type: String
+    },
+    type: {
+        required: true,
+        type: String
+    },
+    disabled: Boolean,
+    id: String
+})
+</script>
+<template>
+    <div>
+        <label :for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" v-text="label" />
+        <input v-model="model" :name :id :type :disabled 
+            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" />
+    </div>
+</template>
