3fadbffe50bc413493ed88e35d5e1e584ed0c9f1
Achmad Rifa'i <janoko.java22@yahoo.com>
2025-03-15 17:04:52 +0700

    changed user photo default

--- src/components/ProfileNav.vue
+++ src/components/ProfileNav.vue
@@ -34,8 +34,7 @@ function out() {
             <button @click.prevent="() => isClose = !isClose" type="button"
                 class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
                 <span class="sr-only">Open user menu</span>
-                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                    alt="user photo">
+                <v-icon class="w-8 h-8 rounded-full" fill="white" name="fa-user" />
             </button>
         </div>
         <div v-if="!isClose" :style="styleDropdown"
