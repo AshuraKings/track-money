ceec0dc88947fb66eed8ce2f02f4b6816a0c5c52
Achmad Rifa'i <janoko.java22@yahoo.com>
2025-03-16 14:55:02 +0700

    changed role menu

--- api/authed/roles/menus/index.go
+++ api/authed/roles/menus/index.go
@@ -1,13 +1,18 @@
 package handler
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 	"track/lib"
 	"track/lib/db"
 	"track/lib/repo"
 	"track/lib/session"
+
+	arrayutils "github.com/AchmadRifai/array-utils"
+	mapsutils "github.com/AchmadRifai/maps-utils"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +37,52 @@ func posting(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
 		panic(err)
 	}
+	validationPost(body)
+	db, err := db.DbConn()
+	defer lib.CloseDb(w, db)
+	if err != nil {
+		panic(err)
+	}
+	tx, err := db.Begin()
+	defer lib.TxClose(tx, w)
+	if err != nil {
+		panic(err)
+	}
+	roleId1, menus1 := body["roleId"].(float64), body["menus"].([]any)
+	roleId, menus := uint64(roleId1), arrayutils.Map(menus1, func(v any, _ int) uint64 { return uint64(v.(float64)) })
+	insertQuery(w, tx, "DELETE FROM role_has_menu WHERE role_id=$1", roleId)
+	for _, m := range menus {
+		insertQuery(w, tx, "INSERT INTO role_has_menu(role_id,menu_id) VALUES($1,$2)", roleId, m)
+	}
 	lib.SendJson(map[string]any{"msg": "Success"}, w)
+}
+
+func insertQuery(w http.ResponseWriter, tx *sql.Tx, query string, args ...any) {
+	stmt, err := tx.Prepare(query)
+	defer lib.StmtClose(w, stmt)
+	if err != nil {
+		panic(err)
+	}
+	_, err = stmt.Exec(args...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func validationPost(body map[string]any) {
+	keys := mapsutils.KeysOfMap(body)
+	for _, k := range []string{"roleId", "menus"} {
+		if !arrayutils.Contains(keys, k) {
+			panic(fmt.Sprintf("bad: %s is required", k))
+		}
+	}
+	roleId, menus := body["roleId"].(float64), body["menus"].([]any)
+	if roleId < 1 {
+		panic("bad: roleId not found")
+	}
+	if len(menus) < 1 {
+		panic("bad: menus is empty")
+	}
 }
 
 func getting(w http.ResponseWriter, r *http.Request) {
